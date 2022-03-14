3bfcaf121b3a41e4be22d2ea719c0035a6e599cc
Tinsae Tariku <tinsaetariku2@gmail.com>
2022-03-14 13:20:02 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,11 +13,11 @@ int main(void)
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
-        {
+	{
 		if (alphabet == 'q')
 			continue;
-	       else if (alphabet == 'e')
-		       continue;
+		else if (alphabet == 'e')
+			continue;
 		putchar(alphabet);
 	}
 
