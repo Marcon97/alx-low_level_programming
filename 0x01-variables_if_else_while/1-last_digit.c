029ff0974ed305ac41040340fb7d5be6981e43c6
Tinsae Tariku <tinsaetariku2@gmail.com>
2022-03-14 07:06:56 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-        int n;
+	int n;
 
         srand(time(0));
         n = rand() - RAND_MAX / 2;
@@ -29,6 +29,3 @@ int main(void)
         }
         return (0);
 }
-
-
-
