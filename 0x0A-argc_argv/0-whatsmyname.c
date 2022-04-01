15037f3e58f77746b77f3c67545c8f7486a8c4d7
Tinsae Tariku <tinsaetariku2@gmail.com>
2022-04-01 14:15:09 -0700

    sub

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - function
- * @argc: offset count
- * @argv: offset value
- * Return: 0
+ * main - Entry Point
+ * @argc : argument counter
+ * @argv : pointer to array
+ * Return:integer
  */
-
-
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - 1]);
-	return (0);
+	exit(EXIT_SUCCESS);
 }
